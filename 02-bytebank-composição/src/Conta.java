c9d408fca84bc8fe4284d6f4d1e807018c6606e6
hugosilva-dev <eahugosilva@gmail.com>
2023-11-26 17:05:47 -0300

    Organizando arquivos

--- 02-bytebank-composição/src/Conta.java
+++ 02-bytebank-composição/src/Conta.java
@@ -17,15 +17,15 @@ public class Conta {
 			return false;
 		}
 	}
-	
-	public boolean transfere(double valor, Conta destino){
-		if(saca(valor)){
+
+	public boolean transfere(double valor, Conta destino) {
+		if (saca(valor)) {
 			destino.deposita(valor);
 			return true;
 		}
 		return false;
 	}
-	
+
 	public double getSaldo() {
 		return this.saldo;
 	}
